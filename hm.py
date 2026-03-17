f7b09c2a577e1f45bf894ae3a1cd3b2df8fe86d9
Valentino12341 <105175770+Valentino12341@users.noreply.github.com>
2026-03-17 16:13:24 +0100

    Add files via upload

---
+++ hm.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+list = [1,2,3,4,5,6,7,8,9,10]
+the_array = np.array([1,2,3,4,5,6,7,8,9,10])
+print(list,"\n\n\n")
+print(the_array,"\n\n\n")
+
+print(list*2,"\n\n\n")
+print(the_array*2)
